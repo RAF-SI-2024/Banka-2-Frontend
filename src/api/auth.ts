119d5764272312036bbbc87e80c5086dd3f004fa
Andjela <andjelaspasojevic081@gmail.com>
2025-02-26 23:45:41 +0100

    Added Register pages for confirming password and activation link

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,13 +1,24 @@
-import api from "./axios";
-import { API_BASE } from "../constants/endpoints";
-import { LoginRequest } from "../types/auth";
+import api from "./axios"
+import { API_BASE } from "../constants/endpoints"
+import { LoginRequest } from "../types/auth"
+import { RegisterRequest } from "../types/auth"
 
 export const loginUser = async (data: LoginRequest) => {
-    try {
-        const response = await api.post(`${API_BASE}/users/login`, data);
-        return response.data; // Returns token or user data
-    } catch (error) {
-        console.error("❌ Login failed:", error);
-        throw error;
-    }
-};
+  try {
+    const response = await api.post(`${API_BASE}/users/login`, data)
+    return response.data // Returns token or user data
+  } catch (error) {
+    console.error("❌ Login failed:", error)
+    throw error
+  }
+}
+
+export const registerUser = async (data: RegisterRequest) => {
+  try {
+    const response = await api.post(`${API_BASE}/users/register`, data)
+    return response.data
+  } catch (error) {
+    console.error("❌ Register failed:", error)
+    throw error
+  }
+}
--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,4 +1,8 @@
 export interface LoginRequest {
-    email: string;
-    password: string;
+  email: string
+  password: string
+}
+
+export interface RegisterRequest {
+  password: string
 }
