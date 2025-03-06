5e8617b2f89b5b0367832e3e6ae2628d0fe8ac50
mradovic38 <mihailo.radovic11@gmail.com>
2025-03-06 18:18:45 +0100

    Updated guards - changed to role based home page.

--- src/router/utils/ProtectedRoutes.tsx
+++ src/router/utils/ProtectedRoutes.tsx
@@ -1,40 +1,19 @@
 import { Outlet, Navigate } from "react-router-dom";
-import {getRoleNumber, Role} from "@/types/enums.ts";
-
-export const AdminProtectedRoutes = () => {
-    const user = sessionStorage.getItem("user");
-    if (user == null)
-        return <Navigate to="/login"/>
-    const token = sessionStorage.getItem("token");
-    const isAdmin = JSON.parse(user).role;
-    return isAdmin == Role.Admin && token != null ? <Outlet/> : <Navigate to="/home" replace/>
-}
 
 export const AllProtectedRoutes = () => {
     const token = sessionStorage.getItem("token");
-    return  token != null ? <Outlet/> : <Navigate to="/login" replace/>
+    const user = sessionStorage.getItem("user");
+
+    if (user == null)
+        return <Navigate to="/login" replace/>
+
+    const role = JSON.parse(user).role;
+
+    return  token != null &&  role != null? <Outlet/> : <Navigate to="/login" replace/>
 }
 
 
 export const ProtectedLoggedUser = () => {
     const token = sessionStorage.getItem("token");
     return  token == null ? <Outlet/> : <Navigate to="/home" replace/>
-}
-
-export const ClientHomePageGuard = () => {
-    const user = sessionStorage.getItem("user");
-    if (user == null)
-        return <Navigate to="/login"/>
-    const token = sessionStorage.getItem("token");
-    const isClient = JSON.parse(user).role;
-    return isClient == Role.Client && token != null ? <Outlet/> : <Navigate to="/home" replace/>
-}
-
-export const EmployeeHomePageGuard = () => {
-    const user = sessionStorage.getItem("user");
-    if (user == null)
-        return <Navigate to="/login"/>
-    const token = sessionStorage.getItem("token");
-    const isEmployee = JSON.parse(user).role;
-    return isEmployee == Role.Employee && token != null ? <Outlet/> : <Navigate to="/home" replace/>
 }
