74c96704deb7dff146189877ce77db0b676e1e49
DusanPajic002 <dusanpajic2002@gmail.com>
2025-03-26 22:55:14 +0100

    Bug fixed

--- src/components/loans/employee/allLoansTable/AllLoanTable.tsx
+++ src/components/loans/employee/allLoansTable/AllLoanTable.tsx
@@ -118,6 +118,7 @@ export default function AllLoanTable() {
 
     // handle filter button click
     const handleFilter = () => {
+        setCurrentPage(1);
         setFetchFlag(!fetchFlag);
     };
 
