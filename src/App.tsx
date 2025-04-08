a2c10d37b1424251613e1253db9d227e5cdf4985
mradovic38 <mihailo.radovic11@gmail.com>
2025-04-08 16:45:47 +0200

    Add suspense to Trading page. Add react query provider. Create ErrorFallback compoennt

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "@/context/AuthContext";
 import { ThemeProvider } from "@/components/__utils__/theme-provider.tsx";
 import {AppRoutes} from "./router/routes";
+import {QueryClient, QueryClientProvider} from "react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
     return (
         <AuthProvider>
             <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+                <QueryClientProvider client={queryClient}>
                     <BrowserRouter>
                         <AppRoutes />
                     </BrowserRouter>
+                </QueryClientProvider>
             </ThemeProvider>
         </AuthProvider>
     );
